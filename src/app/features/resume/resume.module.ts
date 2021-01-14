f09736124ecd429ae91274616bf7c22f73300eaf
César Sá <cesar.sa@innowave.tech>
2021-01-14 18:52:29 +0000

    create containers

--- src/app/features/resume/resume.module.ts
+++ src/app/features/resume/resume.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ResumeRoutingModule } from './resume-routing.module';
+import { ResumeComponent } from './container/resume/resume.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ResumeComponent],
   imports: [
     CommonModule,
     ResumeRoutingModule
